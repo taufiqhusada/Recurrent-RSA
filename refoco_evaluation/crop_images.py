86a124870d074eaf81d34e6e71fe74b3598c84a9
Taufiq Husada Daryanto <13518058@std.stei.itb.ac.id>
2021-07-25 16:28:31 +0700

    update

--- refoco_evaluation/crop_images.py
+++ refoco_evaluation/crop_images.py
@@ -8,8 +8,11 @@ labels_folder_path = '/content/labels'
 
 result_crop_folder_path = '/content/cropped'
 
-dict_list_idx_class = json.load('dict_list_idx_class.json')
-map_idx_to_imgs_id = json.load('map_idx_to_imgs_id.json')
+with open('dict_list_idx_class.json') as jsonFile:
+    dict_list_idx_class = json.load(jsonFile)
+
+with open('map_idx_to_imgs_id.json') as jsonFile:
+    map_idx_to_imgs_id = json.load(jsonFile)
 
 for i in tqdm(range(5000)):
     label = json.load(os.path.join(labels_folder_path, f'lab_{i}.json'))
